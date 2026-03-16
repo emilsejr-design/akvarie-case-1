44865dc462f6b0db2fdf5c267460661e548534c4
emilsejr-design <emilerop@gmail.com>
2026-03-16 11:16:55 +0100

    css commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Spil parameterne 
+let visStartSide = true;
+let score = 0;
+let maxScore = 100;
+
+if(score === maxScore){
+    gameCompleted();
+    }
+
+function updateProgress(){
+    
+}
+
+function removeTrash(trashElement){
+ trashElement.remove();
+ score += 20;
+ updateProgress();
+}
+//Layout af skærmen 
+function setup(){
+}
