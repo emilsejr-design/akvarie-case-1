6b1ec3ac19854bc7e028d7ff2bf09d52ade2283b
kkvonsild-gif <kkvonsild@gmail.com>
2026-03-19 09:54:05 +0100

    JS Index
    Ny script til index.html løste lyd-problem

--- js/script.js
+++ js/script.js
@@ -65,21 +65,6 @@ function createBubble() {
 // Lav mange bobler løbende
 setInterval(createBubble, 300);
 
-
-//LYD
-function playBubbleSound(){
-    let sound = new Audio("sfx-bubbles.mp3");
-    sound.play();
-    }
-
-    // Lyd på FortællerFisken på forsiden
-    const heroFishSpeak = document.getElementById("heltelyd");
-    function playSoundHeroFish() {
-        heroFishSpeak.currentTime = 0;
-        heroFishSpeak.play();
-    }
-    addEventListener("click", playSoundHeroFish);
-
 // TRASH
 // henter skraldeposerne
 const skraldepose1 = document.getElementById("skraldepose1");
