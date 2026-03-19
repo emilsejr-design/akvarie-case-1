a255573f67d59bb6041752ec22cdcac8707824a6
kkvonsild-gif <kkvonsild@gmail.com>
2026-03-19 18:00:23 +0100

    Commit 130?
    Har indsat tang der bevæger sig realistisk og fjernet den akavede lyd på hero fish på ending.html :)

--- js/ending.js
+++ js/ending.js
@@ -3,11 +3,3 @@ window.onload = function playEndingSound(){
     let endingSound = new Audio("mp3/ending-vo.mp3");
     endingSound.play();
     }
-
-    // Lyd på FortællerFisken på forsiden
-    const heroFishSpeak = document.getElementById("heltelyd");
-    function playSoundHeroFish() {
-        heroFishSpeak.currentTime = 0;
-        heroFishSpeak.play();
-    }
-    addEventListener("click", playSoundHeroFish);
